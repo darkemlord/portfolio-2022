7d2731fd81065f69b5f5915463aba16c4b4c1dd1
emanuel caro <darkemlord@gmail.com>
2022-03-06 17:18:49 +0900

    divisions completed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className="App">
       <img className="background-image" src={background} alt="hello" />
-      <h1>hello papi</h1>
+      <div className="grid-division">
+
+      </div>
     </div>
   );
 }
