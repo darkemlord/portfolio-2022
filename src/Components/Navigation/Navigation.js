5a6f2930a157947572ba3edf2c1da271d588562a
emanuel caro <darkemlord@gmail.com>
2022-03-06 23:12:24 +0900

    fixing navegation css

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -8,13 +8,23 @@ const Navigation = () => {
       <div className="profile-container">
         <div className="name-container">
           <h1>Emanuel Caro</h1>
-          <h3>Software <br /> Developer</h3>
+          <div className="dev-cont">
+            <h3>Software</h3>
+            <h3>Developer</h3>
+          </div>
         </div>
         <div className="avatar">
           <img src={emanuel} alt="" />
         </div>
       </div>
       <div className="links">
+        <div className="social">
+          <a href="https://www.facebook.com/emanuel.caro.129"><i className="fa-brands fa-facebook icon"></i></a>
+          <a href="https://github.com/darkemlord"><i className="fa-brands fa-github"></i></a>
+          <a href="https://www.instagram.com/emanuelcaro1/"><i className="fa-brands fa-instagram"></i></a>
+          <a href="https://www.linkedin.com/in/emanuelcaro/"><i className="fa-brands fa-linkedin"></i></a>
+        </div>
+
         <div className="link-navegator">
           <button className="nav-button"><span>About Me</span></button>
           <button className="nav-button"><span>Skills</span></button>
--- src/Components/Social/Social.js
+++ src/Components/Social/Social.js
@@ -3,12 +3,7 @@ import React from 'react'
 const Social = () => {
   return (
     <div>
-      <div className="social">
-          <i className="fa-brands fa-facebook icon"></i>
-          <i className="fa-brands fa-github"></i>
-          <i className="fa-brands fa-instagram"></i>
-          <i className="fa-brands fa-linkedin"></i>
-      </div>
+
     </div>
   )
 }
