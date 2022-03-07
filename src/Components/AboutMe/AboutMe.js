552b5e12904ac854df9f262c94e7f92f01a0fff8
emanuel caro <darkemlord@gmail.com>
2022-03-08 00:55:50 +0900

    button css generated and fixing the box container css

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -10,7 +10,7 @@ const AboutMe = () => {
       <p>
         I have participated in the development of applications such as Toys Market place, Raibu On and Ravenous. I have mainly worked as a lead developer, covering functions of fullstack developer. However, my strong point is the front-end, because I am a professional musician and son of two great artists who taught me the beauty of art. That's why I consider myself a lover of colors and fairy tales.
       </p>
-      <button>Download CV</button>
+      <button className='button-download'>Download CV</button>
     </div>
   )
 }
