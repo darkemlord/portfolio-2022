29c598bec4423a5058251028a8427b461c2f9c16
emanuel caro <darkemlord@gmail.com>
2022-03-07 11:10:54 +0900

    content background effect done

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -4,7 +4,8 @@ const Content = () => {
   return (
     <div className="content">
       <div className="content-container">
-
+        <div className="content-bg"></div>
+        <h1>hola amigos</h1>
       </div>
     </div>
   )
