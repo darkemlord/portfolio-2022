ac0a99744984ae0ff9d321473e0f1bbe36b988bb
emanuel caro <darkemlord@gmail.com>
2022-03-09 22:57:38 +0900

    mobile responsive until 320 max width

--- src/Components/MyProjects/MyProjects.js
+++ src/Components/MyProjects/MyProjects.js
@@ -8,9 +8,9 @@ const MyProjects = () => {
     <div className="projects-container">
       <div className="project-links">
         <button className="button-project"><h2>Raibuon</h2></button>
-        <button className="button-project"><h2>Kids market</h2></button>
-        <button className="button-project"><h2>Mr Cocktails</h2></button>
-        <button className="button-project"><h2>Movie place</h2></button>
+        <button className="button-project"><h2>Kids</h2></button>
+        <button className="button-project"><h2>Cocktails</h2></button>
+        <button className="button-project"><h2>Watch</h2></button>
       </div>
       <div className="project-card">
         <div className="project-image">
