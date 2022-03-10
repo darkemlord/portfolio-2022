9c1c7617ce0fad53fb8333ec0e02f55d7015ac80
emanuel caro <darkemlord@gmail.com>
2022-03-10 13:17:13 +0900

    form working

--- src/Components/ContactMe/ContactMe.js
+++ src/Components/ContactMe/ContactMe.js
@@ -1,14 +1,15 @@
-import './ContactMe.scss'
+import "./ContactMe.scss"
 
 const ContactMe = () => {
+
   return (
-    <div className='contact-container'>
+    <div className="contact-container">
       <div className="form-container">
-        <form action="submit" className='form'>
-          <input type="text" id="name" placeholder=' Name'/>
-          <input type="text" id="email" placeholder=' Email'/>
-          <textarea type="content" id="message" placeholder=' Message'></textarea>
-          <button type="submit">Send</button>
+        <form action="https://getform.io/f/a305fbe2-6f2a-49b7-ad6c-1c2f2e355787" className="form" method="POST">
+          <input type="text" id="name" name="name" placeholder=" Name" required="true"/>
+          <input type="text" id="email" name="email" placeholder=" Email" required="true"/>
+          <textarea type="content" id="message" name="message" placeholder=" Message"></textarea>
+          <button className="button-contact"type="submit">Send</button>
         </form>
       </div>
     </div>
