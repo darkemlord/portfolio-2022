9db8a31acb1c21ddd2347f30d8911ae9b8004581
emanuel caro <darkemlord@gmail.com>
2022-03-10 13:44:18 +0900

    contact form responsive done

--- src/Components/ContactMe/ContactMe.js
+++ src/Components/ContactMe/ContactMe.js
@@ -6,8 +6,8 @@ const ContactMe = () => {
     <div className="contact-container">
       <div className="form-container">
         <form action="https://getform.io/f/a305fbe2-6f2a-49b7-ad6c-1c2f2e355787" className="form" method="POST">
-          <input type="text" id="name" name="name" placeholder=" Name" required="true"/>
-          <input type="text" id="email" name="email" placeholder=" Email" required="true"/>
+          <input type="text" id="name" name="name" placeholder=" Name" required={true}/>
+          <input type="text" id="email" name="email" placeholder=" Email" required={true}/>
           <textarea type="content" id="message" name="message" placeholder=" Message"></textarea>
           <button className="button-contact"type="submit">Send</button>
         </form>
