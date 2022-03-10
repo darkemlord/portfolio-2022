8c32cb8c5002c44a91b03ef088990180adea43b4
emanuel caro <darkemlord@gmail.com>
2022-03-10 10:27:46 +0900

    projects section functionality finished

--- src/Components/MyProjects/MyProjects.js
+++ src/Components/MyProjects/MyProjects.js
@@ -2,37 +2,65 @@ import { useEffect, useState } from 'react'
 import './MyProjects.scss'
 import {images, description} from './projectsData'
 
-console.log(description)
-console.log(images)
-
-
 const MyProjects = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [isClicked, setIsClicket] = useState(false)
+  const [ fade, setFade ] = useState('fade')
+  const [display, setDisplay] = useState(undefined)
+  const [displayDesc, setDisplayDesc] = useState(undefined)
 
   useEffect(() => {
-    const timerId = setInterval(() => {
-      setCount((count) => count + 1)
-    }, 10000);
-    return () => clearInterval(timerId)
-  }, [])
+    if(!isClicked){
+      const timerId = setInterval(() => {
+        setCount((count) => count + 1)
+      }, 7000);
+      return () => clearInterval(timerId)
+    }
+  }, [isClicked])
 
+  const handleClickRaibuon = () => {
+    setIsClicket(true)
+    setDisplayDesc(description[0])
+    setDisplay(images[0])
+    setFade('no-active')
+  }
+  const handleClickKids = () => {
+    setIsClicket(true)
+    setDisplayDesc(description[1])
+    setDisplay(images[1])
+    setFade('no-active')
+  }
+
+  const handleClickCocktails = () => {
+    setIsClicket(true)
+    setDisplayDesc(description[2])
+    setDisplay(images[2])
+    setFade('no-active')
+  }
+
+    const handleClickWatch = () => {
+    setIsClicket(true)
+    setDisplayDesc(description[3])
+    setDisplay(images[3])
+    setFade('no-active')
+  }
   const imagesAll = images[count % images.length]
   const descriptionsAll = description[count % description.length]
 
   return (
     <div className="projects-container">
       <div className="project-links">
-        <button className="button-project"><h2>Raibuon</h2></button>
-        <button className="button-project"><h2>Kids</h2></button>
-        <button className="button-project"><h2>Cocktails</h2></button>
-        <button className="button-project"><h2>Watch</h2></button>
+        <button className="button-project" onClick={handleClickRaibuon}><h2>Raibuon</h2></button>
+        <button className="button-project" onClick={handleClickKids}><h2>Kids</h2></button>
+        <button className="button-project" onClick={handleClickCocktails}><h2>Cocktails</h2></button>
+        <button className="button-project" onClick={handleClickWatch}><h2>Watch</h2></button>
       </div>
       <div className="project-card">
-        <div className="project-image fade">
-          <img src={imagesAll} alt="papis lindos" />
+        <div className={`project-image ${fade}`}>
+          <img src={!display ? imagesAll : display} alt="papis lindos" />
         </div>
-        <div className="project-description fade">
-          <p>{descriptionsAll}</p>
+        <div className={`project-description ${fade}`}>
+          <p>{!displayDesc ? descriptionsAll : displayDesc}</p>
         </div>
       </div>
     </div>
--- src/Components/MyProjects/projectsData.js
+++ src/Components/MyProjects/projectsData.js
@@ -1,12 +1,14 @@
 import raibuon from '../../assets/media/raibuon.png'
 import kidsMarket from '../../assets/media/kidsmarket.png'
 import cocktails from '../../assets/media/cocktails.png'
-
+import watch from '../../assets/media/watch.png'
 const raibuonDesc = "Raibuon is an application designed to facilitate the tipping process of street musicians, through the app you can promote your performance as an artist and also the places where street artists from all over Japan are now playing live. My role in the app was lead developer so I have been taking care of the back-end as well as the front-end, the app is still developing to become a useful product for the artists."
 
 const kidsDesc = "It is an application for mobile devices designed to lend and rent toys that you don't use now and that can be useful for someone else. I have served as lead developer covering parts of the front-end and back-end, we are still working on making improvements and implementing a more efficient lending system."
 
 const cocktailsDesc = "It is a web application compatible also with mobile devices, it is designed to create and remember your cocktail recipes. I have worked on this personal project playing all roles as a developer, many of my friends use it to remember their favorite cocktails, for this reason, I need to make constant updates"
 
-export const images = [raibuon, kidsMarket, cocktails]
-export const description = [raibuonDesc, kidsDesc, cocktailsDesc]
+const watchDesc = "Is an application to share your experiences about your favorite movies, you can also recommend and see the ratings of the best offers there are to see in your free time, you can also add new movies to discuss with other people."
+
+export const images = [raibuon, kidsMarket, cocktails, watch]
+export const description = [raibuonDesc, kidsDesc, cocktailsDesc, watchDesc]
