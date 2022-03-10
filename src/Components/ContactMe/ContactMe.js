9810b47e87a2a04cca3d0e9f1248a5942be53819
emanuel caro <darkemlord@gmail.com>
2022-03-10 11:42:34 +0900

    form basic design done

--- src/Components/ContactMe/ContactMe.js
+++ src/Components/ContactMe/ContactMe.js
@@ -2,8 +2,15 @@ import './ContactMe.scss'
 
 const ContactMe = () => {
   return (
-    <div>
-      <h1>Contact Me</h1>
+    <div className='contact-container'>
+      <div className="form-container">
+        <form action="submit" className='form'>
+          <input type="text" id="name" placeholder=' Name'/>
+          <input type="text" id="email" placeholder=' Email'/>
+          <textarea type="content" id="message" placeholder=' Message'></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
     </div>
   )
 }
