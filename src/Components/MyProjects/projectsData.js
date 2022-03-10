97618b02dd55883b8aa75d06b022ef78d3a01ae0
emanuel caro <darkemlord@gmail.com>
2022-03-10 20:38:39 +0900

    solving my projects size responsive

--- src/Components/MyProjects/projectsData.js
+++ src/Components/MyProjects/projectsData.js
@@ -3,11 +3,11 @@ import kidsMarket from '../../assets/media/kidsmarket.png'
 import cocktails from '../../assets/media/cocktails.png'
 import watch from '../../assets/media/watch.png'
 
-const raibuonDesc = "Raibuon is an application designed to facilitate the tipping process of street musicians, through the app you can promote your performance as an artist and also the places where street artists from all over Japan are now playing live. My role in the app was lead developer so I have been taking care of the back-end as well as the front-end, the app is still developing to become a useful product for the artists."
+const raibuonDesc = "Raibuon is an application designed to facilitate the tipping process of street musicians, through the app you can promote your performance as an artist and also the places where street artists from all over Japan are now playing live."
 
-const kidsDesc = "It is an application for mobile devices designed to lend and rent toys that you don't use now and that can be useful for someone else. I have served as lead developer covering parts of the front-end and back-end, we are still working on making improvements and implementing a more efficient lending system."
+const kidsDesc = "It is an application for mobile devices designed to lend and rent toys that you don't use now and that can be useful for someone else. I have served as lead developer covering parts of the Frontend and Backend."
 
-const cocktailsDesc = "It is a web application compatible also with mobile devices, it is designed to create and remember your cocktail recipes. I have worked on this personal project playing all roles as a developer, many of my friends use it to remember their favorite cocktails, for this reason, I need to make constant updates"
+const cocktailsDesc = "It is a web application compatible also with mobile devices, it is designed to create and remember your cocktail recipes. I have worked on this personal project playing all roles as a developer, many of my friends use it to remember their favorite cocktails."
 
 const watchDesc = "Is an application to share your experiences about your favorite movies, you can also recommend and see the ratings of the best offers there are to see in your free time, you can also add new movies to discuss with other people."
 
