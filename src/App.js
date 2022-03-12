3dcaa80e4731e2b00e326e45afecfe8ba95f3d67
emanuel caro <darkemlord@gmail.com>
2022-03-12 18:32:45 +0900

    removing loading

--- src/App.js
+++ src/App.js
@@ -1,44 +1,21 @@
 import './App.scss'
 import Navigation from './Components/Navigation';
 import Content from './Components/Content'
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import WarningModal from './Components/WarningModal';
-import { RingLoader } from 'react-spinners'
+
 
 
 function App() {
-  const [loading, setLoading] = useState(true)
   const [title, setTitle] = useState('Skills');
-
-  //This useEffect is counting 8 seconds to charge a loading component while the website upload
-
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false)
-    }, 2000);
-  }, [])
 
   return (
     <div className="App">
-      {loading ?
-        <div className="loading-container">
-          <RingLoader
-            size={50}
-            color={'#551596'}
-            loading={loading}
-            />
-            <p className="loading-text">loading</p>
-          </div>
-        :
-        <>
-          <WarningModal />
-          <div className="grid-division">
-            <Navigation titlechange={setTitle}/>
-            <Content title={title} />
-          </div>
-        </>
-      }
+      <WarningModal />
+      <div className="grid-division">
+        <Navigation titlechange={setTitle}/>
+        <Content title={title} />
+      </div>
     </div>
   );
 }
