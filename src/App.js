c590c51210c635f6297934ba6d6c08bc686c8e2e
emanuel caro <darkemlord@gmail.com>
2022-03-12 16:48:59 +0900

    creating a loading function before the app load

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import './App.scss'
 import Navigation from './Components/Navigation';
 import Content from './Components/Content'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import WarningModal from './Components/WarningModal';
 
 
 function App() {
-
+  const [loading, setLoading] = useState(true)
   const [title, setTitle] = useState('About me');
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false)
+    }, 8000);
+  }, [])
 
   return (
     <div className="App">
-      <WarningModal />
-      <div className="grid-division">
-        <Navigation titlechange={setTitle}/>
-        <Content title={title} />
-      </div>
+      {loading ?
+        <h1> hola </h1>
+        :
+        <>
+          <WarningModal />
+          <div className="grid-division">
+            <Navigation titlechange={setTitle}/>
+            <Content title={title} />
+          </div>
+        </>
+      }
     </div>
   );
 }
