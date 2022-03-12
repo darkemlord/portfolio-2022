6b55d6e41bf4e4762e393e1d00be4d829ccafa7b
emanuel caro <darkemlord@gmail.com>
2022-03-12 17:35:00 +0900

    loading container added correctly

--- src/App.js
+++ src/App.js
@@ -3,23 +3,33 @@ import Navigation from './Components/Navigation';
 import Content from './Components/Content'
 import { useState, useEffect } from 'react';
 import WarningModal from './Components/WarningModal';
+import { RingLoader } from 'react-spinners'
 
 
 function App() {
   const [loading, setLoading] = useState(true)
   const [title, setTitle] = useState('About me');
 
+  //This useEffect is counting 8 seconds to charge a loading component while the website upload
+
   useEffect(() => {
     setLoading(true);
     setTimeout(() => {
       setLoading(false)
-    }, 8000);
+    }, 6000);
   }, [])
 
   return (
     <div className="App">
       {loading ?
-        <h1> hola </h1>
+        <div className="loading-container">
+          <RingLoader
+            size={50}
+            color={'#551596'}
+            loading={loading}
+            />
+            <p className="loading-text">loading</p>
+          </div>
         :
         <>
           <WarningModal />
