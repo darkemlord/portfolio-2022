6455e4bab3959891142ae3b5de8301817d904dad
emanuel caro <darkemlord@gmail.com>
2023-01-25 17:13:36 +0900

    updating picture fixing grammar

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,24 +1,24 @@
-import "./Navigation.scss"
-import emanuel from "../../assets/media/emanuels.png"
+import "./Navigation.scss";
+import emanuel from "../../assets/media/emanuel-avatar.png";
 
 const Navigation = (props) => {
-  const { titlechange } = props
+  const { titleChange } = props;
 
   const handleAboutClick = () => {
-    titlechange('About Me');
-  }
+    titleChange("About Me");
+  };
 
   const handleSkillClick = () => {
-    titlechange('Skills');
-  }
+    titleChange("Skills");
+  };
 
   const handleProjectsClick = () => {
-    titlechange('My Projects');
-  }
+    titleChange("My Projects");
+  };
 
   const handleContactMe = () => {
-    titlechange('Contact Me');
-  }
+    titleChange("Contact Me");
+  };
   return (
     <div className="nav-container">
       <div className="profile-container">
@@ -31,25 +31,56 @@ const Navigation = (props) => {
             <img src={emanuel} alt="" />
           </div>
         </div>
-
       </div>
       <div className="links">
         <div className="social">
-          <a href="https://www.facebook.com/emanuel.caro.129" target='_blank' rel="noreferrer"><i className="fa-brands fa-facebook icon"></i></a>
-          <a href="https://github.com/darkemlord" target='_blank' rel="noreferrer"><i className="fa-brands fa-github"></i></a>
-          <a href="https://www.instagram.com/emanuelcaro1/" target='_blank' rel="noreferrer"><i className="fa-brands fa-instagram"></i></a>
-          <a href="https://www.linkedin.com/in/emanuelcaro/" target='_blank' rel="noreferrer"><i className="fa-brands fa-linkedin"></i></a>
+          <a
+            href="https://www.facebook.com/emanuel.caro.129"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa-brands fa-facebook icon"></i>
+          </a>
+          <a
+            href="https://github.com/darkemlord"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa-brands fa-github"></i>
+          </a>
+          <a
+            href="https://www.instagram.com/emanuelcaro1/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa-brands fa-instagram"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/emanuelcaro/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa-brands fa-linkedin"></i>
+          </a>
         </div>
 
-        <div className="link-navegator">
-          <button onClick={handleAboutClick} className="nav-button"><span>About Me</span></button>
-          <button onClick={handleSkillClick} className="nav-button"><span>Skills</span></button>
-          <button onClick={handleProjectsClick} className="nav-button"><span>My projects</span></button>
-          <button onClick={handleContactMe} className="nav-button"><span>Contact Me</span></button>
+        <div className="link-navigator">
+          <button onClick={handleAboutClick} className="nav-button">
+            <span>About Me</span>
+          </button>
+          <button onClick={handleSkillClick} className="nav-button">
+            <span>Skills</span>
+          </button>
+          <button onClick={handleProjectsClick} className="nav-button">
+            <span>My projects</span>
+          </button>
+          <button onClick={handleContactMe} className="nav-button">
+            <span>Contact Me</span>
+          </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
