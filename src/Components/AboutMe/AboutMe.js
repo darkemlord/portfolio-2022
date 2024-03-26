895ea11efd0244149a378f91c69cc5c6c7fccc9b
emanuel caro <darkemlord@gmail.com>
2024-03-26 14:34:05 +0900

    refs#1 upgrading my cv

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -1,21 +1,27 @@
-import './AboutMe.scss'
-import cv from '../../assets/files/EmanuelCaroMarch.pdf'
+import "./AboutMe.scss";
+import cv from "../../assets/files/Emanuel_Samir_Caro_QuistialCV.pdf";
 
 const AboutMe = () => {
   return (
-    <div className='about-container'>
+    <div className="about-container">
       <div className="text-container">
         <p>
-          I am a passionate and driven software developer. I am constantly involved in new projects with the aim to develop quality products.<br /> In order to fuel my creativity and grow my skills I draw inspiration from numerous websites, such as GitHub, and developers around the world. <br/>The world of web development is continuously evolving and I spend my time progressing with it.
+          I am a passionate and driven software developer. I am constantly
+          involved in new projects with the aim to develop quality products.
+          <br /> In order to fuel my creativity and grow my skills I draw
+          inspiration from numerous websites, such as GitHub, and developers
+          around the world. <br />
+          The world of web development is continuously evolving and I spend my
+          time progressing with it.
         </p>
       </div>
       <div className="button-container">
-        <a className='download-container'href={cv} download >
-          <button className='button-download'>Download CV</button>
+        <a className="download-container" href={cv} download>
+          <button className="button-download">Download CV</button>
         </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutMe
+export default AboutMe;
